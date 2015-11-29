0817c04c7817b85bb1489209fe5480dcd95fddb4
Prasanna Krishnasamy <pras.bits@gmail.com>
2015-11-29 01:49:51 -0800

    Adding sample usage of the function
    * minor indentation changes

--- corner_detect.m
+++ corner_detect.m
@@ -1,4 +1,12 @@
 function crnrpts = corner_detect(img, sigma, tau, smoothing_window_size, nms_window_size)
+%%
+% Sample Usage:
+% cpts = corner_detect(imread('bt.001.png'), 1.5, 0.1, 5, 10);
+% I = cpts(:, 1);
+% J = cpts(:, 2);
+% figure, imshow(imread('bt.001.png')), hold on, plot(J, I, 'o'), hold off
+
+%%
 % corner_detect is the refactored version of cornerpts
 if min(size(img)) == 3
     disp('Converting color image to gray')
@@ -16,8 +24,8 @@ img =(img-min(img(:)))/(max(img(:))-min(img(:)));
 halfw=floor(smoothing_window_size/2);
 e=ones(m,n)*tau;
 
-for i = 2 + halfw: n - halfw
-    for j=2+halfw:n-halfw
+for i = 2 + halfw : m - halfw
+    for j = 2 + halfw : n-halfw
         grx_subimg = grx(i-halfw:i+halfw,j-halfw:j+halfw);
         gry_subimg = gry(i-halfw:i+halfw,j-halfw:j+halfw);
         
@@ -37,7 +45,7 @@ crnrpts=[];
 half_nms_win = floor(nms_window_size / 2);
 
 % Non-maximal suppression
-for i = 2 + half_nms_win : n - half_nms_win
+for i = 2 + half_nms_win : m - half_nms_win
     for j=2 + half_nms_win : n-half_nms_win
         sube=e(i-half_nms_win:i+half_nms_win,...
                j-half_nms_win:j+half_nms_win);
